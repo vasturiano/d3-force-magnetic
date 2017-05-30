78c86ccac1b9ecc33be22d61318eb9b164330c3a
vasco <vastur@gmail.com>
2017-05-29 17:18:08 -0700

    Performance improvements

--- src/magnetic.js
+++ src/magnetic.js
@@ -3,45 +3,44 @@ import constant from './constant';
 export default function() {
     let nodes = [],
         links = [],
+        fullMeshLinks = [],
         id = (node => node.index),              // accessor: node unique id
         charge = (node => 100),                   // accessor: number (equivalent to node mass)
-        strength = (link => 1);                 // accessor: 0 <= number <= 1 (equivalent to G constant)
+        strength = (link => 1),                 // accessor: 0 <= number <= 1 (equivalent to G constant)
+        maxDistance = Infinity;
 
     function force(alpha) {
+        const nodeLinks = links.length ? links : fullMeshLinks; // Assume full node mesh if no links specified
 
-        let nodeLinks = links;
+        for (let i=0; i < nodeLinks.length; i++) {
+            const link = nodeLinks[i],
+                dx = link.target.x-link.source.x,
+                dy = link.target.y-link.source.y,
+                d = distance(dx, dy);
 
-        if (!links.length) { // Assume full node mesh
-            nodeLinks = [];
-            nodes.forEach((a, aIdx) => {
-                nodes.filter((_, bIdx) => bIdx > aIdx) // Prevent linking same node pair more than once
-                    .forEach(b => { nodeLinks.push({ source: a, target: b }) });
-            })
-        }
-
-        nodeLinks.forEach(link => {
-            const d = cart2Polar(link.target.x-link.source.x, link.target.y-link.source.y);
-            if (d.d === 0) return;
+            if (d === 0 || d > maxDistance) continue;
 
             // Intensity falls quadratically with distance
-            const relStrength = alpha * strength(link) / (d.d*d.d);
-            const sourceAcceleration = polar2Cart(charge(link.target) * relStrength, d.a);
-            const targetAcceleration = polar2Cart(charge(link.source) * relStrength, d.a + Math.PI);
+            const relStrength = alpha * strength(link) / (d*d);
+            const a = distAngle(dx, dy);
+            const sourceAcceleration = polar2Cart(charge(link.target) * relStrength, a);
+            const targetAcceleration = polar2Cart(charge(link.source) * relStrength, a + Math.PI);
 
             [[link.source, sourceAcceleration], [link.target, targetAcceleration]].forEach(([node, acceleration]) => {
                 node.vx += acceleration.x;
                 node.vy += acceleration.y;
             });
-        });
+        }
 
         //
 
-        function cart2Polar(x, y) {
+        function distance(x, y) {
+            return Math.sqrt(x*x + y*y);
+        }
+
+        function distAngle(x, y) {
             x = x||0; // Normalize -0 to 0 to avoid -Infinity issues in atan
-            return {
-                d: Math.sqrt(x*x + y*y),
-                a: (x === 0 && y === 0) ? 0 : Math.atan(y/x) + (x<0 ? Math.PI : 0) // Add PI for coords in 2nd & 3rd quadrants
-            }
+            return (x === 0 && y === 0) ? 0 : Math.atan(y/x) + (x<0 ? Math.PI : 0); // Add PI for coords in 2nd & 3rd quadrants
         }
 
         function polar2Cart(d, a) {
@@ -62,6 +61,13 @@ export default function() {
             if (typeof link.source !== "object") link.source = nodesById[link.source] || link.source;
             if (typeof link.target !== "object") link.target = nodesById[link.target] || link.target;
         });
+
+        // Reset full-mesh links
+        fullMeshLinks = [];
+        nodes.forEach((a, aIdx) => {
+            nodes.filter((_, bIdx) => bIdx > aIdx) // Prevent linking same node pair more than once
+                .forEach(b => { fullMeshLinks.push({ source: a, target: b }) });
+            });
     }
 
     force.initialize = function(_) {
@@ -76,6 +82,11 @@ export default function() {
     // Node id
     force.id = function(_) {
         return arguments.length ? (id = _, force) : id;
+    };
+
+    // max distance for force to act (saves computation for far-away nodes)
+    force.maxDistance = function(_) {
+        return arguments.length ? (maxDistance = _, force) : maxDistance;
     };
 
     // Node capacity to attract (positive) or repel (negative)
