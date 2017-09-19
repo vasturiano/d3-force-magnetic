a504e7934938c784c9b5c01783e2664cb7ae8796
vasco <vastur@gmail.com>
2017-09-19 01:13:14 -0700

    Babel transpile

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 
 export default {
     plugins: [
         nodeResolve({
             jsnext: true,
             main: true
-        })
+        }),
+        babel()
     ]
 };
