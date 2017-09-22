20b31736d44af5bbfb2720287304ae0af2d890af
vasco <vastur@gmail.com>
2017-09-21 22:05:33 -0700

    Upgrade rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,21 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
 export default {
+    entry: 'src/index.js',
+    dest: 'dist/d3-force-magnetic.js',
+    format: 'umd',
+    moduleName: 'd3',
     plugins: [
         nodeResolve({
             jsnext: true,
             main: true
         }),
-        babel()
+        babel({
+            presets: [
+                ["es2015", { "modules": false }]
+            ],
+            plugins: ["external-helpers"],
+            babelrc: false
+        })
     ]
 };
