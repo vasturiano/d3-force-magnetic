f940501648e9b3850f2fcdbd447d67a4ae8491c7
vasco <vastur@gmail.com>
2021-03-16 01:10:30 +0000

    Upd pkg build

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,55 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonJs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import { name, homepage, version } from './package.json';
+import { terser } from 'rollup-plugin-terser';
+import { name, homepage, version, dependencies } from './package.json';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      format: 'umd',
-      extend: true,
-      name: 'd3',
-      file: `dist/${name}.js`,
-      sourcemap: true,
-      banner: `// Version ${version} ${name} - ${homepage}`
-    }
-  ],
-  plugins: [
-    resolve(),
-    commonJs(),
-    babel({ exclude: 'node_modules/**' })
-  ]
+const umdConf = {
+  format: 'umd',
+  extend: true,
+  name: 'd3',
+  banner: `// Version ${version} ${name} - ${homepage}`
 };
+
+export default [
+  { // UMD
+    input: 'src/index.js',
+    output: [
+      {
+        ...umdConf,
+        file: `dist/${name}.js`,
+        sourcemap: true
+      },
+      { // minify
+        ...umdConf,
+        file: `dist/${name}.min.js`,
+        plugins: [terser({
+          output: { comments: '/Version/' }
+        })]
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonJs(),
+      babel({ exclude: 'node_modules/**' })
+    ]
+  },
+  { // commonJs and ES modules
+    input: 'src/magnetic.js',
+    output: [
+      {
+        format: 'cjs',
+        file: `dist/${name}.common.js`,
+        exports: 'auto'
+      },
+      {
+        format: 'es',
+        file: `dist/${name}.module.js`
+      }
+    ],
+    external: Object.keys(dependencies),
+    plugins: [
+      babel()
+    ]
+  },
+];
